ed0af4aa8771ce6d599d289226352f7cf9a4472f
新都心(Neet Shin) <nucx@dio-vox.com>
2017-06-03 10:25:40 +0530

    Update terms.php

--- public_html/login/terms.php
+++ public_html/login/terms.php
@@ -1,73 +1,128 @@
+
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Terms / Halcyon</title>
-  <link rel="shortcut icon" href="/login/assets/images/favicon.ico" />
-  <link rel="stylesheet" type="text/css" href="/login/assets/css/terms.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="/assets/css/fonts.css" media="all" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="/assets/js/mastodon.js/mastodon.js"></script>
-  <script src="/assets/js/jquery-cookie/src/jquery.cookie.js"></script>
-  <script src="/assets/js/functions.js"></script>
-
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-98782992-1', 'auto');
-    ga('send', 'pageview');
-  </script>
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Halcyon for Mastodon</title>
+    <link rel="shortcut icon" href="/assets/images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="/login/assets/css/style.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/fonts.css" media="all" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="/assets/js/jquery-cookie/src/jquery.cookie.js"></script>
+    <script src="/login/assets/js/halcyon_login.js"></script>
 </head>
-  <body>
-    <main>
-      <article id="terms">
 
-<div class="icon_box">
-  <a href="/">
-    <img src="/login/assets/images/halcyon-title.png">
-  </a>
-</div>
+<body>
 
-<h1>Halcyon Terms of Use</h1>
-<p class="description">This terms of use agreement is for the users of web service Halcyon for Mastodon (Halcyon for short) hosted at <a href="https://halcyon.social">halcyon.social</a>.</p>
-<br/>
-<h2>1. Agreement</h2>
-<p>By logging into Halcyon you agree to this terms of use agreement. Do not use this service if you do not agree to these terms.</p>
-<br/>
-<h2>2. Change</h2>
-<p>We reserve the right to modify these terms of use at any time if deemed necessary.</p>
-<br/>
-<h2>3. Privacy</h2>
-<p>Halcyon uses <a href="https://www.google.com/analytics/">Google Analytics</a> to record and analyze statistics of usage. It stores cookies in the browser storage for this purpose. However, this can be disabled by browser setting.</p>
-<br/>
-<h2>4. Account</h2>
-<p>Users can connect to various Mastodon accounts from Halcyon, and Halcyon will not interfere with those connection.</p>
-<br/>
-<h2>5. Content</h2>
-<p>Halcyon does not own the right to or hold responsibility for any text, image, animation, audio content accessible from Halcyon. The right and responsibility belong to the owners of instances that host those content.</p>
-<br/>
-<h2>6. Guideline</h2>
-<p>Halcyon reserves the right to terminate service for</p>
-<ul>
-  <li>actions violating the laws of countries</li>
-  <li>actions contrary to public order and standards of decency</li>
-  <li>violation of third party’s right to intellectual property, brand, privacy, etc.</li>
-  <li>acts of violence, sexual nature, of discrimination</li>
-  <li>phishing or spamming</li>
-  <li>actions causing problem to the network infrastructure of Halcyon</li>
-</ul>
-<br/>
-<h2>7. Disclaimer</h2>
-<p>Halcyon is not responsible for any damage caused by using this service.</p>
-<br/>
-<p>Last updated 2017/5/12</p>
+    <header id="header">
 
-      </article>
+      <div id="header_wrap">
+
+        <div id="header_title_wrap" class="header_box header_right_box">
+
+            <div class="header_box_child title_box">
+                <a href="/">
+                    <!-- TITLE IMAGE -->
+                    <img src="/login/assets/images/halcyon-title.png" alt="Halcyon for mastodon"/>
+                </a>
+            </div>
+
+        </div>
+
+        <div id="header_menu_wrap" class="header_box header_left_box">
+
+            <nav class="header_box_child nav_box">
+                <ul>
+
+                    <!-- NEWS-->
+                    <a href="https://mastodon.social/@halcyon" class="no-underline">
+                        <li>
+                            <span><i class="fa fa-newspaper-o" aria-hidden="true"></i>News</span>
+                        </li>
+                    </a>
+
+                    <!-- SOURCE-->
+                    <a href="https://github.com/halcyon-suite" class="no-underline">
+                        <li>
+                            <span><i class="fa fa-code" aria-hidden="true"></i>Source</span>
+                        </li>
+                    </a>
+
+                    <!-- TERMS -->
+                    <a class="no-underline">
+                        <li>
+                            <span><i class="fa fa-balance-scale" aria-hidden="true"></i>Terms</span>
+                        </li>
+                    </a>
+
+                    <!-- CONTACT -->
+                    <a href="mailto:neetshin@neetsh.in" class="no-underline">
+                        <li>
+                            <span><i class="fa fa-envelope" aria-hidden="true"></i>Contact</span>
+                        </li>
+                    </a>
+
+                    <!-- SIGN IN -->
+                    <a href="#login_form_wrap" class="no-underline">
+                        <li>
+                            <span><i class="fa fa-user-circle-o" aria-hidden="true"></i>Login</span>
+                        </li>
+                    </a>
+
+                </ul>
+            </nav>
+
+        </div>
+
+      </div>
+
+    </header>
+
+    <main id="main">
+        <article id="article">
+            <h1>Halcyon Terms of Use</h1>
+            <p class="description">This terms of use agreement is for the users of web service Halcyon for Mastodon (Halcyon for short) hosted at <a href="https://halcyon.social">halcyon.social</a>.</p>
+            <br/>
+            <h2>1. Agreement</h2>
+            <p>By logging into Halcyon you agree to this terms of use agreement. Do not use this service if you do not agree to these terms.</p>
+            <br/>
+            <h2>2. Change</h2>
+            <p>We reserve the right to modify these terms of use at any time if deemed necessary.</p>
+            <br/>
+            <h2>3. Account</h2>
+            <p>Users can connect to various Mastodon accounts from Halcyon, and Halcyon will not interfere with those connection.</p>
+            <br/>
+            <h2>4. Content</h2>
+            <p>Halcyon does not own the right to or hold responsibility for any text, image, animation, audio content accessible from Halcyon. The right and responsibility belong to the owners of instances that host those content.</p>
+            <br/>
+            <h2>5. Guideline</h2>
+            <p>Halcyon reserves the right to terminate service for</p>
+            <ul>
+              <li>actions violating the laws of countries</li>
+              <li>actions contrary to public order and standards of decency</li>
+              <li>violation of third party’s right to intellectual property, brand, privacy, etc.</li>
+              <li>acts of violence, sexual nature, of discrimination</li>
+              <li>phishing or spamming</li>
+              <li>actions causing problem to the network infrastructure of Halcyon</li>
+            </ul>
+            <br/>
+            <h2>6. Disclaimer</h2>
+            <p>Halcyon is not responsible for any damage caused by using this service.</p>
+            <br/>
+            <p>Last updated 2017/5/12</p>
+        </article>
+
     </main>
-  </body>
-</html>
+
+    <footer id="footer">
+        <div class="footer_anchor">
+            <a href="#">
+                <i class="fa fa-angle-up" aria-hidden="true"></i>
+            </a>
+        </div>
+        <span>Photo by <a href="https://www.flickr.com/photos/95387826@N08/">Michio Morimoto on Flickr</a> (CC BY 2.0)</span>
+    </footer>
+    
+</body>
