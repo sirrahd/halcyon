cbe2b0c13c0fa4553bb793305cc9aa75396b5186
NeetShin <nucx@dio-vox.com>
2017-06-03 10:40:11 +0530

    remove directory

--- public_html/assets/js/halcyon.js/footer.js
+++
@@ -1,5 +0,0 @@
-setCurrentProfile();
-$('.header_settings_link').attr('href','https://'+current_instance+'/settings/preferences');
-$('.footer_widget_about').attr('href','https://'+current_instance+'/about');
-$('.footer_widget_instance').attr('href','https://'+current_instance+'/about/more');
-$('.footer_widget_terms').attr('href','https://'+current_instance+'/terms');
