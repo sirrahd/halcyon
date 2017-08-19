3db2288da672a519835955bb73107948ddbb6c97
neetshin <neetshin@neetsh.in>
2017-08-19 14:22:14 +0900

    Add Gemfile

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
 ini_set("error_log", "../log/error.log");
-require_once __DIR__."/../Dispatcher.php";
+require_once "dispatcher.php";
 $dispatcher = new Dispatcher();
 $dispatcher->dispatch();
