3653472c94c8cc7907728c7ce020b9273cd90ea5
neetshin <neetshin@neetsh.in>
2017-09-06 03:55:12 +0900

    Routed controller dirs

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -7,7 +7,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class NotificationController extends BaseController
+class NotificationsController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/",              "HomeController");
+Route::get("/local",         "LocalController");
+Route::get("/federated",     "FederatedController");
+Route::get("/notifications", "NotificationsController");
+Route::get("/login",         "LoginController");
+Route::get("/logout",        "LogoutController");
+
+Route::get("/{acct}",              "ProfileController")->where("acct", "/^@(.+)@(.+)\.(.+)$/");
+Route::get("/{acct}/following",    "ProfileController@following")->where("acct", "/^@(.+)@(.+)\.(.+)$/");
+Route::get("/{acct}/followers",    "ProfileController@followers")->where("acct", "/^@(.+)@(.+)\.(.+)$/");
+Route::get("/{acct}/favourites",   "ProfileController@favourites")->where("acct", "/^@(.+)@(.+)\.(.+)$/");
+Route::get("/{acct}/with_replies", "ProfileController@withReplies")->where("acct", "/^@(.+)@(.+)\.(.+)$/");
+Route::get("/{acct}/media",        "ProfileController@media")->where("acct", "/^@(.+)@(.+)\.(.+)$/");
+
+// Alias
+Route::redirect("/home", "/", 301);
