da32b7a62221128b4bf30b0735e5ae65e5242fb9
neetshin <neetshin@neetsh.in>
2017-09-06 04:38:03 +0900

    Created base view template

---
+++ resources/views/base/appBase.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield("title")</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/stylesheet/style.css" />
+        <script src="/javascript/main.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
