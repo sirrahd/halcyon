8e9d4ffd94e6370e60aec34bcab7d1e78daa9445
NeetShin <neetshin@neetsh.in>
2017-11-13 12:57:58 +0530

    Delete locale directory

--- config/mastodon.php
+++ config/mastodon.php
@@ -5,18 +5,15 @@ return [
     // Name of your application
     'client_name' => "Haclyon for Mastodon",
 
-    // Where the user should be redirected after authorization
-    // (for no redirect, use urn:ietf:wg:oauth:2.0:oob)
+    // Path to redirect after authorization
     'redirect_uris' => [
         url('/login'),
         'urn:ietf:wg:oauth:2.0:oob'
     ],
 
-    // This can be a space-separated list of the following items:
-    // "read", "write" and "follow" (see this page for details on what the scopes do)
+    // Scope app requests
     'scopes' => ['read', 'write', 'follow'],
 
-    // URL to the homepage of your app
-    // * optional config *
+    // Optional: URL of app
     'website' => url('/'),
 ];
