7fd6855e55692e9e9b47206ab26715cf7806ffb0
NeetShin <neetshin@neetsh.in>
2017-11-25 23:43:54 +0530

    Make sync valid languages between Laravel to react-intl

--- app/Http/Middleware/LanguageDetector.php
+++ app/Http/Middleware/LanguageDetector.php
@@ -21,19 +21,17 @@ class LanguageDetector
      */
     public function handle($request, Closure $next)
     {
-        $known_languages = ['en', 'ja'];
         $query_lang  = $request->input('lang');
         $cookie_lang = $request->cookie('lang');
+        $known_langs = $this->getKnownLanguages('../../../resources/javascript/locales');
 
-        if ( $query_lang && in_array($query_lang, $known_languages) ) {
+        if ( $query_lang && in_array($query_lang, $known_langs) ) {
             $lang = $query_lang;
-        } else if ( $cookie_lang && in_array($cookie_lang, $known_languages) ) {
+        } else if ( $cookie_lang && in_array($cookie_lang, $known_langs) ) {
             $lang = $cookie_lang;
         } else if ( !is_null($request->server('HTTP_ACCEPT_LANGUAGE')) ) {
             $accept_langs = array_filter(
-                array_map(
-                    function($tag)
-                    {
+                array_map(function($tag) {
                         $exploded = explode(';', $tag );
                         $trimed   = array_map('trim', $exploded);
                         return array_shift($trimed);
@@ -42,7 +40,7 @@ class LanguageDetector
                 )
             );
             foreach ( $accept_langs as &$accept_lang ) {
-                if ( in_array($accept_lang, $known_languages) ) {
+                if ( in_array($accept_lang, $known_langs) ) {
                     \Cookie::queue(cookie('lang', $accept_lang, time()+365*24*3600));
                     $lang = $accept_lang;
                     break;
@@ -57,5 +55,22 @@ class LanguageDetector
         \App::setLocale($lang);
 
         return $next($request);
+    }
+
+    /**
+     * Detect locale files from react-intl locale directory
+     *
+     * @param  $path  path to react-intl locale directory
+     * @return array  array of valid language
+     */
+    protected function getKnownLanguages($path) {
+        $files = scandir($path);
+        $return = [];
+        foreach ($files as $file_name) {
+            if (preg_match('/(.+?)\.json$/', $file_name, $pure_file_name)) {
+                $return[] = $pure_file_name[1];
+            }
+        }
+        return $return;
     }
 }
