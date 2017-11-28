038464a7c1fbea1eb16bcbf08a5a9cdd9d035593
NeetShin <neetshin@neetsh.in>
2017-11-28 17:00:54 +0530

    Migrationを手動にした

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
-grep -E '^APP_KEY=base64:.+?=$' /halcyon/.env
-if [ $? -eq 1 ]; then
-    php artisan key:generate
-fi
-
-php artisan migrate
-php artisan route:cache
-php artisan config:cache
-
 php-fpm -D
 nginx -g "daemon off;"
