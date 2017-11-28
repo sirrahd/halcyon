b48af04f2339d23b4bd1229c5613c81e4dcbb216
NeetShin <neetshin@neetsh.in>
2017-11-28 13:56:27 +0530

    Make detect APP_KEY is defined

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-php artisan key:generate
-php artisan migrate
+grep -E '^APP_KEY=base64:.+?=' /halcyon/.env
+
+if [ $? -eq 1 ]; then
+    php artisan key:generate
+    php artisan migrate
+fi
+
 php artisan route:cache
 php artisan config:cache
 nginx -g "daemon off;"
