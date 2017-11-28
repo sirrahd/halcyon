763546528e4f7a0e7d4ba27a84f7c9837b6cfcbc
NeetShin <neetshin@neetsh.in>
2017-11-28 13:59:45 +0530

    Revert migrate command out of the nest

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,9 +3,10 @@ grep -E '^APP_KEY=base64:.+?=' /halcyon/.env
 
 if [ $? -eq 1 ]; then
     php artisan key:generate
-    php artisan migrate
 fi
 
+php artisan migrate
 php artisan route:cache
 php artisan config:cache
+
 nginx -g "daemon off;"
