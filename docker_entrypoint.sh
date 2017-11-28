030c191b1bb194b408db3619a30a0843a461b173
NeetShin <neetshin@neetsh.in>
2017-11-28 14:41:03 +0530

    Add php-fpm start command

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 grep -E '^APP_KEY=base64:.+?=$' /halcyon/.env
-
 if [ $? -eq 1 ]; then
     php artisan key:generate
 fi
 
+## Optimize some settings
 php artisan migrate
 php artisan route:cache
 php artisan config:cache
 
+## Starting php-fpm
+php-fpm
+
+## Starting nginx
 nginx -g "daemon off;"
