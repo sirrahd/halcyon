389a18fa5f36433778a3ecca7afa2f14c260de7a
NeetShin <neetshin@neetsh.in>
2017-12-01 19:02:17 +0530

    Update index view template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,15 +4,15 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}" id="csrf-token">
         <title>Halcyon</title>
         <link rel="shortcut icon" href="favicon.ico" />
         <link rel="stylesheet" href="{{ mix('/stylesheet/theme_light.css') }}" />
     </head>
     <body class="{{ app()->getLocale() }}" role="application">
-        <div id="halcyon" data-props="{{json_encode([
+        <div id="halcyon" data-props="{{ json_encode([
             'locale' => app()->getLocale()
-        ])}}"></div>
+        ]) }}"></div>
         <script src="{{ mix('/javascript/main.js') }}"></script>
     </body>
 </html>
