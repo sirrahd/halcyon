b501c29820c21226e582a811c7328bc2f1cd86f4
NeetShin <neetshin@neetsh.in>
2017-12-02 15:50:32 +0530

    Apply redux-thunk

--- resources/javascript/store/configureStore.js
+++ resources/javascript/store/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import appReducer from '../reducers';
+import thunk from 'redux-thunk';
 import logger from '../middlewares/logger';
 import searchMiddleware from '../middlewares/searchMiddleware';
 
 function configureStore() {
   return createStore(appReducer, applyMiddleware(
+    thunk,
     logger,
     searchMiddleware,
   ));
