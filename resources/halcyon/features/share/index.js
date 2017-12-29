1cbdb93d9ae195312c84d88910f42827a30c0880
NeetShin <neetshin@neetsh.in>
2017-12-29 15:19:01 +0530

    Update share page

--- resources/halcyon/features/share/index.js
+++ resources/halcyon/features/share/index.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { changeCompose } from '../../actions/compose';
+import { FormattedMessage } from 'react-intl';
+import { withRouter } from 'react-router-dom';
+import qs from 'qs';
 
 import Page from '../app/components/page';
 import Content from '../app/components/content';
-import Dashborad from '../app/components/dashboard';
 import ComposeFormContainer from '../compose/containers/compose_form_container';
 
+const mapDispatchToProps = dispatch => ({
+  onMount(text) {
+    dispatch(changeCompose(text));
+  },
+});
+
+@connect(null, mapDispatchToProps)
+@withRouter
 export default class Share extends React.PureComponent {
 
+  static propTypes = {
+    location: PropTypes.object.isRequired,
+    onMount: PropTypes.func.isRequired,
+  }
+
   componentWillMount () {
-    // dispatch ?text query to the store
+    const text = qs.parse(this.props.location.search.substr(1)).text || '';
+    this.props.onMount(text);
   }
 
   render () {
     return (
       <Page>
         <Content>
-          <Dashborad position='left' />
+          <div className='share'>
+            <h3 className='share__title'>
+              <FormattedMessage id='share.title' defaultMessage='Share link to your followers' />
+            </h3>
 
-          <div>
             <ComposeFormContainer />
           </div>
-
-          <Dashborad position='right' />
         </Content>
       </Page>
     );
