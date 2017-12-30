a5febd7d76c290c60f1a9bf1bd6da4464d1d1c60
NeetShin <neetshin@neetsh.in>
2017-12-30 13:34:56 +0530

    Fix navigation's shouldComponentUpdate

--- resources/halcyon/features/topbar/components/page_navigation.js
+++ resources/halcyon/features/topbar/components/page_navigation.js
@@ -1,50 +1,53 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { NavLink, withRouter } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
-const mapStateToProps = state => ({
-  showNavigationLabels: state.getIn(['settings', 'halcyon', 'showNavigationLabels']),
-});
-
-@connect(mapStateToProps)
 @withRouter
 export default class PageNavigation extends React.Component {
 
   static propTypes = {
     location: PropTypes.object.isRequired,
-    showNavigationLabels: PropTypes.bool.isRequired,
   }
 
-  shouldComponentUpdate(nextProps) {
+  shouldComponentUpdate (nextProps) {
     if ( this.props.location.pathname !== nextProps.location.pathname ) {
       return true;
     }
     return false;
   }
 
-  renderItem = (itemName, linkTo, iconClass, messageId) => (
-    <li key={itemName} className='page-navigation__list-item'>
-      <NavLink exact to={linkTo} className='page-navigation__link' activeClassName='page-navigation__link--current'>
-        <i className={`page-navigation__icon ${iconClass}`} />
-        <FormattedMessage id={messageId} defaultMessage={itemName} />
-      </NavLink>
-    </li>
-  )
-
   render() {
-    const items = [
-      ['home', '/timelines/home', 'icon-home', 'page_navigation.home'],
-      ['local', '/timelines/public/local', 'icon-users', 'page_navigation.local'],
-      ['federated', '/timelines/public', 'icon-social', 'page_navigation.federated'],
-      ['notifications', '/notifications', 'icon-bell', 'page_navigation.notifications'],
-    ];
-
     return (
       <nav className='page-navigation'>
         <ul className='page-navigation__list'>
-          { items.map(item => this.renderItem(...item)) }
+          <li className='page-navigation__list-item'>
+            <NavLink exact to='/timelines/home' className='page-navigation__link' activeClassName='page-navigation__link--current'>
+              <i className='page-navigation__icon icon-home' />
+              <FormattedMessage id='page_navigation.home' defaultMessage='Home' />
+            </NavLink>
+          </li>
+
+          <li className='page-navigation__list-item'>
+            <NavLink exact to='/timelines/public/local' className='page-navigation__link' activeClassName='page-navigation__link--current'>
+              <i className='page-navigation__icon icon-users' />
+              <FormattedMessage id='page_navigation.local' defaultMessage='Local' />
+            </NavLink>
+          </li>
+
+          <li className='page-navigation__list-item'>
+            <NavLink exact to='/timelines/public' className='page-navigation__link' activeClassName='page-navigation__link--current'>
+              <i className='page-navigation__icon icon-social' />
+              <FormattedMessage id='page_navigation.federated' defaultMessage='Federated' />
+            </NavLink>
+          </li>
+
+          <li className='page-navigation__list-item'>
+            <NavLink exact to='/notifications' className='page-navigation__link' activeClassName='page-navigation__link--current'>
+              <i className='page-navigation__icon icon-bell' />
+              <FormattedMessage id='page_navigation.notifications' defaultMessage='Notifications' />
+            </NavLink>
+          </li>
         </ul>
       </nav>
     );
