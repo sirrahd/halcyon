29890023ad6297b89b1b7032d4626275fbc56369
Neetshin <neetshin@neetsh.in>
2018-01-02 19:59:03 +0530

    Add domain exportation

--- resources/halcyon/initial_state.js
+++ resources/halcyon/initial_state.js
@@ -9,6 +9,7 @@ export const autoPlayGif   = getMeta('auto_play_gif');
 export const unfollowModal = getMeta('unfollow_modal');
 export const boostModal    = getMeta('boost_modal');
 export const deleteModal   = getMeta('delete_modal');
+export const domain        = getMeta('domain');
 export const me            = getMeta('me');
 
 export default initialState;
