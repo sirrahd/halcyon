bfcee6e70c09948efdb7e3e5e19edbc7d85ac9be
Neetshin <neetshin@neetsh.in>
2018-01-04 11:35:33 +0530

    Update Hashtag timeline

--- resources/halcyon/features/hashtag_timeline/index.js
+++ resources/halcyon/features/hashtag_timeline/index.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import ImmutablePureComponent from 'react-immutable-pure-component';
+import { refreshHashtagTimeline, expandHashtagTimeline } from '../../actions/timelines';
+import { me } from '../../initial_state';
 
-const HashtagTimeline = () => (
-  <main className='page-container' />
-);
+import Page from '../app/components/page';
+import Content from '../app/components/content';
+import Dashborad from '../app/components/dashboard';
+import ProfileCard from '../../containers/profile_card_container';
 
-export default HashtagTimeline;
+import Timeline from '../../components/timeline';
+import TimelineHeaderCompose from '../../components/timeline_header_compose';
+import StatusListContainer from '../../containers/status_list_container';
+
+@connect()
+export default class HashtagTimeline extends React.Component {
+
+  static propTypes = {
+    params: PropTypes.object,
+    dispatch: PropTypes.func.isRequired,
+  }
+
+  componentWillMount () {
+    this.props.dispatch(refreshHashtagTimeline(this.props.params.id));
+  }
+
+  render() {
+    const { id } = this.props.params;
+
+    return (
+      <Page>
+        <Content>
+          <Dashborad position='left'>
+            <ProfileCard accountId={me} withCounters />
+          </Dashborad>
+
+          <Timeline>
+            <TimelineHeaderCompose />
+            <StatusListContainer timelineId={`hashtag:${id}`} />
+          </Timeline>
+
+          <Dashborad position='right' />
+        </Content>
+      </Page>
+    );
+  }
+
+}
