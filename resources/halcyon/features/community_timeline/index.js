8fbfc5a6b7c29fac9a502062e219adc30295cb79
Neetshin <neetshin@neetsh.in>
2018-01-04 11:35:24 +0530

    Update Local timeline

--- resources/halcyon/features/community_timeline/index.js
+++ resources/halcyon/features/community_timeline/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import ImmutablePureComponent from 'react-immutable-pure-component';
+import { refreshCommunityTimeline, expandCommunityTimeline } from '../../actions/timelines';
 import { me } from '../../initial_state';
 
-export default class CommunityTimeline extends React.PureComponent {
+import Page from '../app/components/page';
+import Content from '../app/components/content';
+import Dashborad from '../app/components/dashboard';
+import ProfileCard from '../../containers/profile_card_container';
 
-  componentDidMount() {
-    console.log(me);
+import Timeline from '../../components/timeline';
+import TimelineHeaderCompose from '../../components/timeline_header_compose';
+import StatusListContainer from '../../containers/status_list_container';
+
+@connect()
+export default class CommunityTimeline extends React.Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+  }
+
+  componentWillMount () {
+    this.props.dispatch(refreshCommunityTimeline());
   }
 
   render() {
     return (
-      <main className='page-container' />
+      <Page>
+        <Content>
+          <Dashborad position='left'>
+            <ProfileCard accountId={me} withCounters />
+          </Dashborad>
+
+          <Timeline>
+            <TimelineHeaderCompose />
+            <StatusListContainer timelineId='community' />
+          </Timeline>
+
+          <Dashborad position='right' />
+        </Content>
+      </Page>
     );
   }
 
