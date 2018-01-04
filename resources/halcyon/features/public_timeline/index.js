2484fdb390fc03f1fcd8a4c7ba44db970f582ef9
Neetshin <neetshin@neetsh.in>
2018-01-04 11:36:25 +0530

    Update Federated timeline

--- resources/halcyon/features/public_timeline/index.js
+++ resources/halcyon/features/public_timeline/index.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import ImmutablePureComponent from 'react-immutable-pure-component';
+import { refreshPublicTimeline, expandPublicTimeline } from '../../actions/timelines';
+import { me } from '../../initial_state';
 
-const Federated = () => (
-  <main className='page-container' />
-);
+import Page from '../app/components/page';
+import Content from '../app/components/content';
+import Dashborad from '../app/components/dashboard';
+import ProfileCard from '../../containers/profile_card_container';
 
-export default Federated;
+import Timeline from '../../components/timeline';
+import TimelineHeaderCompose from '../../components/timeline_header_compose';
+import StatusListContainer from '../../containers/status_list_container';
+
+@connect()
+export default class PublicTimeline extends React.Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+  }
+
+  componentWillMount () {
+    this.props.dispatch(refreshPublicTimeline());
+  }
+
+  render() {
+    return (
+      <Page>
+        <Content>
+          <Dashborad position='left'>
+            <ProfileCard accountId={me} withCounters />
+          </Dashborad>
+
+          <Timeline>
+            <TimelineHeaderCompose />
+            <StatusListContainer timelineId='public' />
+          </Timeline>
+
+          <Dashborad position='right' />
+        </Content>
+      </Page>
+    );
+  }
+
+}
