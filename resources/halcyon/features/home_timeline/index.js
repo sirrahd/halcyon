ce5b23d9763bdc694c25db4079cb21bdbcfc0b54
Neetshin <neetshin@neetsh.in>
2018-01-04 11:36:13 +0530

    Update Home timeline

--- resources/halcyon/features/home_timeline/index.js
+++ resources/halcyon/features/home_timeline/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
-// import ImmutablePropTypes from 'react-immutable-proptypes';
-// import ImmutablePureComponent from 'react-immutable-pure-component';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import ImmutablePureComponent from 'react-immutable-pure-component';
+import { refreshHomeTimeline, expandHomeTimeline } from '../../actions/timelines';
 import { me } from '../../initial_state';
 
 import Page from '../app/components/page';
 import Content from '../app/components/content';
 import Dashborad from '../app/components/dashboard';
 import ProfileCard from '../../containers/profile_card_container';
+
 import Timeline from '../../components/timeline';
 import TimelineHeaderCompose from '../../components/timeline_header_compose';
+import StatusListContainer from '../../containers/status_list_container';
 
+@connect()
 export default class HomeTimeline extends React.Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+  }
+
+  componentWillMount () {
+    this.props.dispatch(refreshHomeTimeline());
+  }
 
   render() {
     return (
@@ -22,7 +36,7 @@ export default class HomeTimeline extends React.Component {
 
           <Timeline>
             <TimelineHeaderCompose />
-            <StatusListContainer />
+            <StatusListContainer timelineId='home' />
           </Timeline>
 
           <Dashborad position='right' />
